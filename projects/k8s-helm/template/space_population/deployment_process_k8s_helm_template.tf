476e298128419e2a1e5fa890a9bc050adab45c36
mcasperson <mcasperson@users.noreply.github.com>
2024-07-14 20:46:42 +0000

    Apply automatic changes

--- projects/k8s-helm/template/space_population/deployment_process_k8s_helm_template.tf
+++ projects/k8s-helm/template/space_population/deployment_process_k8s_helm_template.tf
@@ -55,9 +55,9 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
       is_required                        = false
       worker_pool_id                     = ""
       properties                         = {
-        "Octopus.Action.Manual.Instructions" = "Proceed?"
         "Octopus.Action.RunOnServer" = "false"
         "Octopus.Action.Manual.BlockConcurrentDeployments" = "False"
+        "Octopus.Action.Manual.Instructions" = "Proceed?"
       }
       environments                       = []
       excluded_environments              = []
@@ -84,10 +84,10 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
       can_be_used_for_project_versioning = true
       is_required                        = false
       properties                         = {
-        "Octopus.Action.Package.DownloadOnTentacle" = "False"
-        "Octopus.Action.RunOnServer" = "false"
         "Octopus.Action.Helm.ClientVersion" = "V3"
         "Octopus.Action.Helm.ResetValues" = "True"
+        "Octopus.Action.Package.DownloadOnTentacle" = "False"
+        "Octopus.Action.RunOnServer" = "false"
       }
       environments                       = []
       excluded_environments              = []
