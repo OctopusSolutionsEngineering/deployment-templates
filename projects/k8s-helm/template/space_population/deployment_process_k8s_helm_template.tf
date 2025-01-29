869c4e58a96956a9e68959542c9225793805fd4e
mcasperson <160104+mcasperson@users.noreply.github.com>
2025-01-29 05:02:33 +0000

    Apply automatic changes

--- projects/k8s-helm/template/space_population/deployment_process_k8s_helm_template.tf
+++ projects/k8s-helm/template/space_population/deployment_process_k8s_helm_template.tf
@@ -17,7 +17,7 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
     action {
       action_type                        = "Octopus.Manual"
       name                               = "Manual Intervention Required"
-      notes                              = ""
+      notes                              = "Prompt to continue"
       condition                          = "Success"
       run_on_server                      = false
       is_disabled                        = false
@@ -25,9 +25,9 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
       is_required                        = false
       worker_pool_id                     = ""
       properties                         = {
-        "Octopus.Action.RunOnServer" = "false"
         "Octopus.Action.Manual.BlockConcurrentDeployments" = "False"
         "Octopus.Action.Manual.Instructions" = "Proceed?"
+        "Octopus.Action.RunOnServer" = "false"
       }
       environments                       = []
       excluded_environments              = []
@@ -54,10 +54,10 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
       can_be_used_for_project_versioning = true
       is_required                        = false
       properties                         = {
-        "Octopus.Action.Helm.ClientVersion" = "V3"
         "Octopus.Action.Helm.ResetValues" = "True"
         "Octopus.Action.Package.DownloadOnTentacle" = "False"
         "Octopus.Action.RunOnServer" = "false"
+        "Octopus.Action.Helm.ClientVersion" = "V3"
       }
       environments                       = []
       excluded_environments              = []
@@ -94,10 +94,10 @@ resource "octopusdeploy_deployment_process" "deployment_process_k8s_helm_templat
       is_required                        = false
       worker_pool_id                     = "${data.octopusdeploy_worker_pools.workerpool_hosted_windows.worker_pools[0].id}"
       properties                         = {
+        "Octopus.Action.Script.ScriptSource" = "Inline"
         "Octopus.Action.Script.Syntax" = "PowerShell"
         "Octopus.Action.RunOnServer" = "true"
         "Octopus.Action.Script.ScriptBody" = "Write-host \"hello\""
-        "Octopus.Action.Script.ScriptSource" = "Inline"
       }
       environments                       = []
       excluded_environments              = []
